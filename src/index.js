814c302e0745e09c96085e812118d8f9ca18b0ea
森 <913168497@qq.com>
2024-08-06 11:35:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const routes = {
   "ecr.ggdzpj.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ggdzpj.cn": dockerHub,
 };
 
 function routeByHosts(host) {
